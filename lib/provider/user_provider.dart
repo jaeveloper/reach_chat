c5500d66db40dae04dabe6498c922360a1195d75
Joshua Ukpozi <jukpoz@gmail.com>
2020-12-03 12:38:17 +0100

    first commit

---
+++ lib/provider/user_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/widgets.dart';
+import 'package:reach_voip/models/user.dart';
+import 'package:reach_voip/resources/auth_methods.dart';
+
+class UserProvider with ChangeNotifier {
+  User _user;
+  AuthMethods _authMethods = AuthMethods();
+
+  User get getUser => _user;
+
+  void refreshUser() async {
+    User user = await _authMethods.getUserDetails();
+    _user = user;
+    notifyListeners();
+  }
+}
